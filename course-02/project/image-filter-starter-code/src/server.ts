038fd135e6f7f8e312a901d2d235124aed6b06c6
weiguo xia <wxia61@gmail.com>
2019-08-05 15:04:22 -0400

    finished

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,15 +1,17 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +32,32 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+
+  app.get("/filteredimage", async (req, res) => {
+    let image_url = req.query.image_url;
+    let result = filterImageFromURL(image_url);
+    result.then(
+      function(result) {
+        res.sendFile(result, function(err) {
+          if (err) {
+          } else {
+            let results: Array<string> = [result];
+            deleteLocalFiles(results);
+          }
+        });
+      },
+      function(err) {
+        console.log(err); // Error: "It broke"
+      }
+    );
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
